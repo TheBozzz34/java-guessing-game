8013ccee58cf42607522097fdefa5b5789252f94
Ethan James <lskywalker416@gmail.com>
2023-02-15 19:49:01 -0700

    optomize imports

--- src/main/java/xyz/necrozma/Main.java
+++ src/main/java/xyz/necrozma/Main.java
@@ -2,8 +2,7 @@ package xyz.necrozma;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import xyz.necrozma.GameLogic;
-import xyz.necrozma.GameGui;
+
 public class Main {
     private static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
